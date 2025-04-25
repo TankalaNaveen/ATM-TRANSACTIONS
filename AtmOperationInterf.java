e9e88bb6e627288acc8d539dff9a9b4d9925512c
TankalaNaveen <naveentankala9@gmail.com>
2025-04-25 21:23:16 +0530

    Create AtmOperationInterf.java

---
+++ AtmOperationInterf.java
@@ -0,0 +1,15 @@
+package atm;
+
+
+
+public interface AtmOperationInterf {
+
+	public void viewBalance();
+
+	public void withdrawAmount(double withdrawAmount);
+
+	public void depositAmount(double depositAmount);
+
+	public void viewMiniStatement();
+
+}
